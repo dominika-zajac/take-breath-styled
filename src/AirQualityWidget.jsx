f34088cd6d4512587afa7a7536b32b2a0d98aa50
dominika-zajac <dominikazajac00@gmail.com>
2022-05-29 14:14:04 +0200

    Initial commit

---
+++ src/AirQualityWidget.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { fetchAirData } from './api/airQuality.api';
+import "./styles/air-quality-widget.css"
+
+
+const AirQualityWidget = ({ city }) => {
+  const [airData, setAirData] = useState(null);
+  useEffect(() => {
+    fetchAirData(city).then((fetchedAirData) => {
+      setAirData(fetchedAirData);
+    });
+  }, []);
+
+  const refreshAirData = () => {
+    fetchAirData(city).then((fetchedAirData) => {
+      setAirData(fetchedAirData);
+    });
+  }
+
+  return airData === null ? null : (
+    <div className="air-quality-widget-container">
+      <div className="air-quality-city">{city}</div>
+      <div className="air-quality-info-container">
+        <div className="air-quality-aqi">{airData.aqi}</div>
+        <div className="air-quality-label">{airData.quality}</div>
+      </div>
+      <button className="air-quality-refresh-button" onClick={refreshAirData}>refresh</button>
+    </div>
+  );
+};
+
+export default AirQualityWidget;
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AirQualityContainer from './AirQualityContainer';
+import Divider from './Divider';
+import Image from './Image';
+import AirQualtiyWidget from './AirQualityWidget';
+import './styles/app.css';
+import Title from './Title';
+
+export default function App() {
+  return (
+    <div class="main-container">
+      <Title title="Take a breath" />
+      <Image />
+      <AirQualityContainer>
+        <AirQualtiyWidget city="Cracow" />
+        <AirQualtiyWidget city="London" />
+        <AirQualtiyWidget city="Beijing" />
+      </AirQualityContainer>
+    </div>
+  );
+}
