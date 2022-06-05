cd10d7ec079fdb7844eed6933c358a2735c99926
lukw34 <lukw34@gmail.com>
2022-06-05 15:34:53 +0200

    Step 4

--- src/AirQualityWidget.jsx
+++ src/AirQualityWidget.jsx
@@ -4,6 +4,13 @@ import "./styles/air-quality-widget.css"
 
 export default function AirQualityWidget () {
     return (
-        <div/>
+        <div className="air-quality-widget-container">
+        <div className="air-quality-city">Cracow</div>
+        <div className="air-quality-info-container">
+            <div className="air-quality-aqi">17</div>
+            <div className="air-quality-label">Good</div>
+        </div>
+            <button className="air-quality-refresh-button" onClick={() => alert("Air data refreshed")}>refresh</button>
+        </div>
     );
 }
