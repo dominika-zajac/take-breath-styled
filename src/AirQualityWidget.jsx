eff3f5d9e21fb5972daffe7d882ab319231016f8
lukw34 <lukw34@gmail.com>
2022-06-05 15:39:43 +0200

    Step 5

--- src/AirQualityWidget.jsx
+++ src/AirQualityWidget.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./styles/air-quality-widget.css"
+import { fetchAirData } from './api/airQuality.api';
 
-
-export default function AirQualityWidget () {
+export default function AirQualityWidget ({ city, aqi, airQuality }) {
     return (
-        <div className="air-quality-widget-container">
-        <div className="air-quality-city">Cracow</div>
+    <div className="air-quality-widget-container">
+        <div className="air-quality-city">{city}</div>
         <div className="air-quality-info-container">
-            <div className="air-quality-aqi">17</div>
-            <div className="air-quality-label">Good</div>
+           <div className="air-quality-aqi">{aqi}</div>
+           <div className="air-quality-label">{airQuality}</div>
         </div>
-            <button className="air-quality-refresh-button" onClick={() => alert("Air data refreshed")}>refresh</button>
-        </div>
+        <button className="air-quality-refresh-button" onClick={() => alert("Air data refreshed")}>refresh</button>
+    </div>
     );
 }
