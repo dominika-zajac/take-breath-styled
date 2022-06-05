d7b7521276c34f0f0d5024af928dbf602d6bb72b
lukw34 <lukw34@gmail.com>
2022-06-05 15:52:33 +0200

    Step 7

--- src/AirQualityWidget.jsx
+++ src/AirQualityWidget.jsx
@@ -3,12 +3,10 @@ import "./styles/air-quality-widget.css"
 import { fetchAirData } from './api/airQuality.api';
 
 export default function AirQualityWidget ({ city }) {
-    let aqi = 0
-    let airQuality = "Default"
+    const [airData, setAirData] = useState({ aqi: 0, airQuality: "Default"});
     useEffect(() => {
       fetchAirData(city).then((fetchedAirData) => {
-        aqi = fetchedAirData.aqi
-        airQuality = fetchedAirData.airQuality
+        setAirData(fetchedAirData);
       });
     }, []);
 
@@ -17,8 +15,8 @@ export default function AirQualityWidget ({ city }) {
     <div className="air-quality-widget-container">
         <div className="air-quality-city">{city}</div>
         <div className="air-quality-info-container">
-           <div className="air-quality-aqi">{aqi}</div>
-           <div className="air-quality-label">{airQuality}</div>
+           <div className="air-quality-aqi">{airData.aqi}</div>
+           <div className="air-quality-label">{airData.airQuality}</div>
         </div>
         <button className="air-quality-refresh-button" onClick={() => alert("Air data refreshed")}>refresh</button>
     </div>
